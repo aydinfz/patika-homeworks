7046e5b51c71b36d19ac80b8de2bee98e1bcd0c7
aydinfz <95143820+aydinfz@users.noreply.github.com>
2022-06-26 14:35:48 +0300

    merge sort completed

---
+++ Data-structures-and-algorithms/merge-sort.js
@@ -0,0 +1,18 @@
+// Merge Sort
+/*
+
+[16,21,11,8,12,22]
+
+1) Write the stages of the above array according to merge sort.
+
+[16,21,11,8,12,22]
+[16,21,11] - [8,12,22]
+[16] - [21,11] - [8] - [12,22]
+[16] - [21] - [11] - [8] - [12] - [22]
+[11,16,21] - [8,12,22]
+[8,11,12,16,21,22]
+
+2) Write the Big-O notation.
+O(nlogn)
+
+*/
